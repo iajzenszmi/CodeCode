5bc1d76a4cd7fdd1a3604926303136b6a3cde144
iajzenszmi <iajzenszmi@gmail.com>
2018-04-04 19:43:42 +1000

    Add files via upload

---
+++ f7.f
@@ -0,0 +1,8 @@
+
+        integer seven
+        integer fourteen
+        seven = 7
+        fourteen = 2 * seven
+        write(6,9000) fourteen
+ 9000   format(""," fourteen = ",i2)
+        end program
