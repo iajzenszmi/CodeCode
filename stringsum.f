25911f2e2df0d671605160070ea554f69c81a236
iajzenszmi <iajzenszmi@gmail.com>
2018-06-17 17:01:50 +1000

    Add files via upload

--- stringsum.f
+++ stringsum.f
@@ -1,12 +1,11 @@
        integer istring(5), icount, sumistring
-
-      data istring /1,2,3,4,5/
+       data istring /1,2,3,4,5/
           sumistring = 0
        do 100 icount = 1,5,1
           sumistring = sumistring+ istring(icount)
 100    continue
-           print *, sumistring
-           write(6,9000) sumistring
-9000      format(" ",i10)           
+!           print *, sumistring
+           write(6,9000) sumistring,(istring(icount),icount=1,5)
+9000      format(" ",i10 " = sum of " ,5(i10, " "))           
           end
 
