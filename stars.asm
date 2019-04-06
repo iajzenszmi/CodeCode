33045d8035b5856f288b8afdd9642e434dee75b5
iajzenszmi <iajzenszmi@gmail.com>
2019-04-06 15:47:43 +1100

    Add files via upload

--- stars.asm
+++ stars.asm
@@ -1,20 +1,25 @@
 section .text
-global _start ;must be declared.) 
-_start: ;tell linker entry point
-mov edx, len ; message length
-mov ecx, msg ; message to write
-mov ebx, 1 ;file descriptor (stdout)
-mov eax, 4 ;system call number (sys_write)  
-int 0x80 ; call kernel
-mov edx, len2 ;message length
-mov ecx,s2 ;message to write
-mov ebx, 1 ;file descriptor (stdout)
-mov eax, 4 ;system call number (sys_write)
-int 0x80   ;call kernel
-mov eax, 1  ; system call number (sys_exit)
-int 0x80 ;call kernel
+	global _start	;must be declared for the linker (ld or gcc)
+
+start:		; telling the linker the entry point of the code
+	
+	mov edx, len	; message length
+	mov ecx, msg    ; message to write
+	mov ebx, 1      ; file descriptor (stdout)
+	mov eax, 4      ; system call number for writing
+	int 0x80        ; calling the kernel
+
+	mov edx, 9      ; message length
+	mov ecx, s2     ; message to write
+	mov ebx, 1      ; file descriptor to stdout
+	mov eax, 4      ; system call for writing
+	int 0x80        ; calling the kernel
+
+	mov eax, 1      ; system call number for exiting
+	int 0x80        ; call kernel
+
 section .data
-msg: db 'Displaying 9 stars' ;a message
-len: equ $-msg ;length of message
-s2:  db '*********',
-len2: equ $-s2 
+msg db 'Displaying 9 stars bro, such a feat P' 
+len equ $ - msg
+s2  db '*********'
+len2 equ $ - s2
