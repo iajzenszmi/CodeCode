556da2a11121c2f256c7dffcebab7e0e00ab838c
iajzenszmi <iajzenszmi@gmail.com>
2020-09-11 06:44:01 +1000

    Add files via upload

---
+++ fourfortran.f
@@ -0,0 +1,8 @@
+         real(4)::four = 4.0
+!         real(2):: four2 = 4.0
+         real(8)::four8 = 4.0
+         real(16):: four16 = 4.0
+!         real(32):: four32 = 4.0
+!         real(64):: four64 = 4.0
+         print *, four,  four8,  four16
+         end program
