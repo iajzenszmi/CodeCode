68cb07556d68eec861c397af4cdfacd79222a3a7
iajzenszmi <iajzenszmi@gmail.com>
2020-10-12 14:08:39 +1100

    Add files via upload

---
+++ cppcon1to3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int i;
+int main(){
+   for (int i = 0;i < 3; ++i){
+	   std::cout << i << '\n';
+	   }
+	   }
+//from timur Doumlers talk CPPCON 2020 ON YOUTUBE.
+// HOW C++20 Changes the way we write code.
