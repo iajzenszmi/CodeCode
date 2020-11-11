3690171925a76f5238f7d4078ac02c570dd7240f
iajzenszmi <iajzenszmi@gmail.com>
2020-11-11 13:57:14 +1100

    Add files via upload

---
+++ addagoodone.f
@@ -0,0 +1,6 @@
+        integer one 
+        one = 1
+        print *,"spell it out and add it up"
+        sum = one + 1
+        print *, " sum = ",sum , " one " , one, " + 1"
+        end program
