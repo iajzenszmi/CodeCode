abf3b2577e9299d666f3cd088bedcc41b48036fa
iajzenszmi <iajzenszmi@gmail.com>
2020-12-28 20:40:32 +1100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,8 @@
+import scipy
+print("hello world")
+print(2+2)
+print(scipy.math.sin(90))
+print("multiplication tables")
+for i in range(1,13):
+    for j in range(1,13):
+	        print(i,'*',j," = ",i*j) 
