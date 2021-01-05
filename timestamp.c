f893477e925f54886b06b1a90bffbb1cf217226d
iajzenszmi <iajzenszmi@gmail.com>
2021-01-06 10:33:33 +1100

    Add files via upload

---
+++ timestamp.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(){
+  
+unsigned long long int msr;
+
+asm volatile ( "rdtsc\n\t"    // Returns the time in EDX:EAX.
+        "shl $32, %%rdx\n\t"  // Shift the upper bits left.
+        "or %%rdx, %0"        // 'Or' in the lower bits.
+        : "=a" (msr)
+        : 
+        : "rdx");
+
+printf("%s%llx\n","x0", msr);
+}
