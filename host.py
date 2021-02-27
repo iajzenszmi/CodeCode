fbc30978587c395ef6da955a885f78fb34ad5b10
iajzenszmi <iajzenszmi@gmail.com>
2021-02-27 23:09:29 +1100

    Add files via upload

---
+++ host.py
@@ -0,0 +1,6 @@
+import qsharp
+from Qrng import SampleQuantumRandomNumberGenerator
+from Qrng import SampleRandomNumber
+
+print(SampleQuantumRandomNumberGenerator.simulate())
+print(SampleRandomNumber.simulate())
