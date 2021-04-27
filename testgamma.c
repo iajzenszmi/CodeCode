b168333d7ad878dbc2c298e86fe9e2ee4398233c
Ian Martin Ajzenszmidt <iajzenszmi@gmail.com>
2021-04-28 00:29:20 +1000

    Add files via upload

---
+++ testgamma.c
@@ -0,0 +1,20 @@
+/* tgamma example */
+#include <stdio.h>      /* printf */
+#include <math.h>       /* tgamma */
+
+int main ()
+{
+ // double param, result;
+//  param = 0.5;
+  printf("\n%10.2f",tgamma(1.0));
+  printf("\n%10.2f",tgamma(2.0));
+  printf("\n%10.2f",tgamma(3.0));
+  printf("\n%10.2f",tgamma(4.0));
+  printf("\n%10.2f",tgamma(5.0));
+  printf("\n%10.2f",tgamma(6.0));
+  printf("\n%10.2f",tgamma(7.0));
+  printf("\n%10.2f",tgamma(8.0));
+  printf("\n%10.2f",tgamma(9.0));
+  printf("\n%10.2f",tgamma(10.0));
+  return 0;
+}
---
+++ testgamma.f08
@@ -0,0 +1,14 @@
+program test_gamma
+  print *,gamma(1.0)
+  print *,gamma(2.0)
+  print *,gamma(3.0)
+  print *,gamma(4.0)
+  print *,gamma(5.0)
+  print *,gamma(6.0)
+  print *,gamma(7.0)
+  print *, gamma(8.0)
+  x = gamma(x)  ! returns 1.0
+  
+  print *, x, gamma(x)
+  
+end program test_gamma
