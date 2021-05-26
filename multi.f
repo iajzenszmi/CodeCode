9118b90c58dbdc49279713d0d2822f7063abc071
Ian Martin Ajzenszmidt <iajzenszmi@gmail.com>
2021-05-27 03:34:22 +1000

    Add files via upload

---
+++ multi.f
@@ -0,0 +1,13 @@
+           integer mult1
+           integer mult2
+           integer mult3
+           print *,"enter multiplier  :"
+           read *, mult1
+           print *,"enter multiplicand"
+           read *, mult2
+           mult3 = mult1 * mult2
+           print *,mult1
+           print *,mult2
+           print *,"the answer is "
+           print *,mult3
+           end program
