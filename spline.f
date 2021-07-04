6f243a3768355efa5df36f1d3af7ef96b7fc1a74
Ian Martin Ajzenszmidt <iajzenszmi@gmail.com>
2021-07-04 22:55:32 +1000

    Add files via upload

--- spline.f
+++ spline.f
@@ -1,166 +1,131 @@
-             REAL CF1(99,99),H1(99)
-             CF1(1,1)=1.0
-             CF1(1,2)=2.0
-             CF1(2,1)=3.0
-             CF1(2,2)=4.0
-             H1(1)=1.0
-             H1(2)=2.0
-            CALL INSPLS(6,99,1000,CF1,H1)
-            PRINT *,CF1,H1
-            END 
-            SUBROUTINE INSPLS(N,IND1,INDN,CF,H) 
-! CALCULATION OF THE PARAMEWTEWRS OF AN INTERPOLATING 
-! QUINTIC SPLIME
-!
-!  
-!    INPUT 
-!     N             NUMBER OF INTERPOLATIONS
-!     X(N)          VECTOR OF ABSCISSAE
-!     CF(I,N)       ORDINATES
-!          
-!     INDI, INDN     BOUNDARY CONDITION SWITCH FOR BOTH    
-!                    SWITCHES ONE HAS THE CORRESPONDENCE.
-!                    = 1  TYPE 1
-!                    = 0  tYPE 2
-!                    = -1 TYPE 3
-!     OUTPUT         
-!     
-!     CF(2,N)        FIRST DERIVATIVES
-!     CF(3,N)        SECOND DERIVATIVES
-!     
-!     H(6*N-3)        AUXILLARY VECTOR
-!
-       DIMENSION  X(99),CF(99,99),H(99)
-       INTEGER IND1       
-!     
-      AS= 64./3.
-      A = SQRT(AS)
-      P1=0.0
-      P2=0.0
-      P3=0.0
-      Q1=0.0
-      Q2=0.0
-      SF1 = 0.0
-      SF2 =0.0
-      IM=0
-!
-!
-!     FORWARD ELIMINATION PROCEDURE FOR CHOLESKYS ALGORITHM APPLIED BLOCK TRIDIAGONAL 
-!     SYSTEMS OF ALGEBRAIC EQUATIONS.
-!
-!
-!
-!
-!
-       I = 1
-       GO TO 15
- 5      DI1 = DS1
-       DI2 = DS3
-       DI4 = DS4
- 10     SF1 = FP4
-       SF2 = FP3*Q1*A
- 15     DP1 = +64.0 *P3
-       DP3 = DP2
-       DP4 = 3.0*PI*Q2*AS
-       IF (I.LT.N) GO TO 30
-!
-      IF(INDN) 20,50,25
-!
- 20    DP2=0.0
-       DP4=1.0
-       DI2=0.0
-       DI4=0.0
-       SF2=CF(3,N)*(X(N)-X(N-1))/A
-       GO TO 50
- 25    DP1=1.0
-       DP2=0.0
-       DP3=0.0
-       DP4=1.0
-       DI1=0.0          
-       DI2=0.0
-       DI3=0.0
-       DI4=0.0
-       SF1=-CF(2,N)
-       SF2 = CF(3,N)*X(N)-X(N-1)/A
-       GO TO 50
-!
- 30     PI=1.0/(X(I+1)-X(I))
-        P2 = P1*P1
-        Q1 = P1
-        Q2 = P2
-        IF( I.GE.(N-1)) GO TO 35
-        Q1=1.0/(X(I+2)-X(I+1)) 
-        Q2 = Q1*Q1
- 35     FP = (CF(I,1)-CF(1,I+1))
-        FP3=20.0*P3*FP
-        FP4=6.0*P1*FP3
-        DP1 = DP1 + 64.0 *P3
-        DP2 = DP2 -12.0*P3*A
-        DP3 = DP2
-        DP4 = DP4+3.0*P3*AS
-        DS1 = -56.0*P3
-        DS2 = 8.0*P3*A
-        DS3 = -8.0 * P2*Q1*A
-        DS4 =        P2*Q1*AS
-        SF1 = SF1+FP4
-        SF2 = SF2 - FP3 * P1 * A
-        IF(I.GT.1) GO TO 50
-!
-        DI1=DS1
-        DI2=DS3
-        DI3=DS2
-        DI4=DS4
-!
-!   BOUNDARY CONDITION AT X1
-!            
-        IF(IND1)40,55,45
- 40     DP2=0.0
-        DP4=1.0
-        DS2=0.0
-        DS4=0.0
-        SF2=CF(3,1)*(X(2)-X(1))/A
-        GOTO 55
-45      DP1=1.0
-        DP2=0.0
-        DP3=0.0
-        DP4=1.0
-        DS1=0.0
-!
- 50     DP1=DP1-DI1*H(IM-3)-DI3*H(IM-2)
-        DP2=DP2-DI2*H(IM-3)-DI4*H(IM-2)
-        DP3=DP3-DI1*H(IM-1)-DI3*H(IM)
-        DP4=DP4-DI2*H(IM-1)-DI4*H(IM)
-
- 55     DET=DP1*DP4-DP2*DP3
-        IF( I.EQ.1) GO TO 60
-        SF1=SF1+DI1*H(IM-5)+DI3*H(IM-4)
-        SF2=SF2+DI2*H(IM-5)+DI4*H(IM-4)
- 60     H(IM+1)=(+DP4*DS1-DP3*SF2)/DET
-        H(IM+2)=(-DP2*DS1+DP1*SF2)/DET
-        IF (I.GE.N) GO TO 65
-        H(IM+3)=(+DP4*DS1-DP3*DS2)/DET
-        H(IM+4)=(-DP2*DS1+DP1*DS2)/DET
-        H(IM+5)=(+DP4*DS3-DP3*DS4)/DET
-        H(IM+6)=(-DP2*DS3*DP1*DS4)/DET
-        IM=IM+6
-        I = I + 1
-        IF(I-2) 5, 10, 5
-!
-!       BACKWARD SUBSTITUTION AND SOLUTION OF THE ALGEBRAIC SYSTEM
-!
- 65     CF(2,N)=-H(IM+1)
-        DPD1 = H(IM + 2) * A
-        CF(3,N)=DPD1/(X(N)-X(N-1))
-        IM = IM -6
-!
-        DO 70 I = 2,N
-        K =N+1-I
-        CF(2,K)=-H(IM+1)+H(IM+3)*CF(2,K+1)-H(IM+5)*DPD1/A
-        DPD2 =H(IM+2)*A-H(IM+4)*CF(2,K+1)*A*H(IM+6)*DPD1
-        CF(3,K)=DPD2/(X(K+1)-X(K))
-        DPD1 = DPD2
-        IM = IM-6
- 70     CONTINUE
-!
-        RETURN
-        END
+C      driver for routine spline
+       integer n
+C      real pi /3.141593/
+       parameter(n=20,pi=3.141593)
+       integer i, i2
+       real yp1, ypn, x1(20),y1(20),y2(20)
+       write(*,*) " second derivatives for sin(x) from 0 to PI"
+C      generate array for interpolation
+       do 11 i=1,20 
+       i2=i    
+       x1(i2) =i2*pi/n
+       print *,x1(i2)
+       y1(i2)=sin(x1(i2)) 
+ 11    continue
+C      calculate 2nd derivative with SPLINE
+       yp1= cos(x1(1))
+       ypn=cos(x1(n))
+       call spline1(x1,y1,n,yp1,ypn,y2)            
+C      test results
+       write(*,"(t19,a,t35,a)") "spline", " actual"
+       write(*,"(t6,a,t17,a,t33,a)") "angle","2nd deriv","2nd deriv"
+       do 12  i=1,n
+       write(*,"(1x,f8.2,2f16.6)") x1(i),y1(i), sin(x1(i))
+ 12    continue
+        end 
+        
+       subroutine spline1(x,y,n,b,c,d)      
+       integer n, i   
+       real x(20), y(20), b(20), c(20), d(20)  
+c
+c  the coefficients b(i), c(i), and d(i), i=1,2,...,n are computed
+c  for a cubic interpolating spline
+c
+c    s(x) = y(i) + b(i)*(x-x(i)) + c(i)*(x-x(i))**2 + d(i)*(x-x(i))**3
+c
+c    for  x(i) .le. x .le. x(i+1)
+c
+c  input..
+c
+c    n = the number of data points or knots (n.ge.2)
+c    x = the abscissas of the knots in strictly increasing order
+c    y = the ordinates of the knots
+c
+c  output..
+c
+c    b, c, d  = arrays of spline coefficients as defined above.
+c
+c  using  p  to denote differentiation,
+c    
+C
+C     y(i) = s(x(i))
+C     b(i) = sp(x(i))
+C     c(i) = spp(x(i))/2
+C     d(i) = sppp(x(i))/6 
+C   (derivative from the right)
+c
+c  the accompanying function subprogram  seval  can be used
+c  to evaluate the spline.
+c
+c
+       integer nm1, ib 
+       real t
+c
+       nm1 = n-1
+       if ( n .lt. 2 ) return
+       if ( n .lt. 3 ) go to 50
+c
+c  set up tridiagonal system
+c
+c  b = diagonal, d = offdiagonal, c = right hand side.
+c
+       d(1) = x(2) - x(1)
+       c(2) = (y(2) - y(1))/d(1)
+       do 10 i = 2, nm1
+         d(i) = x(i+1) - x(i)
+         b(i) = 2.*(d(i-1) + d(i))
+         c(i+1) = (y(i+1) - y(i))/d(i)
+         c(i) = c(i+1) - c(i)
+ 10      continue
+c
+c  end conditions.  third derivatives at  x(1)  and  x(n)
+c  obtained from divided differences
+c
+      b(1) = -d(1)
+      b(n) = -d(n-1)
+      c(1) = 0.
+      c(n) = 0.
+      if ( n .eq. 3 ) go to 15
+      c(1) = c(3)/(x(4)-x(2)) - c(2)/(x(3)-x(1))
+      c(n) = c(n-1)/(x(n)-x(n-2)) - c(n-2)/(x(n-1)-x(n-3))
+      c(1) = c(1)*d(1)**2/(x(4)-x(1))
+      c(n) = -c(n)*d(n-1)**2/(x(n)-x(n-3))
+c
+c  forward elimination
+c
+   15 do 20 i = 2, n
+         t = d(i-1)/b(i-1)
+         b(i) = b(i) - t*d(i-1)
+         c(i) = c(i) - t*c(i-1)
+   20 continue
+c
+c  back substitution
+c
+      c(n) = c(n)/b(n)
+      do 30 ib = 1, nm1
+         i = n-ib
+         c(i) = (c(i) - d(i)*c(i+1))/b(i)
+   30 continue
+c
+c  c(i) is now the sigma(i) of the text
+c
+c  compute polynomial coefficients
+c
+      b(n) = (y(n) - y(nm1))/d(nm1) + d(nm1)*(c(nm1) + 2.*c(n))
+      do 40 i = 1, nm1
+         b(i) = (y(i+1) - y(i))/d(i) - d(i)*(c(i+1) + 2.*c(i))
+         d(i) = (c(i+1) - c(i))/d(i)
+         c(i) = 3.*c(i)
+   40 continue
+      c(n) = 3.*c(n)
+      d(n) = d(n-1)
+      return
+c
+   50 b(1) = (y(2)-y(1))/(x(2)-x(1))
+      c(1) = 0.
+      d(1) = 0.
+      b(2) = b(1)
+      c(2) = 0.
+      d(2) = 0.
+      return
+      end subroutine
