9dade76c67df5b5bd139faa65f11267974a605a8
Ian Martin Ajzenszmidt <iajzenszmi@gmail.com>
2021-07-30 23:55:15 +1000

    Add files via upload

--- reada.f
+++ reada.f
@@ -1,46 +1,53 @@
        character(len=80) inrec
        character(len=4) year, yearx
+       character(len=2) month,day
        integer io 
        integer reccount 
        logical eof
 !       integer n /0/
        integer k
        eof = .false.
-       reccount = 0
+       reccount = 1
        io = 0
-!       do while(.not.eof)
        open(5,file="IDCJAC0009_086304_1800_Data.csv")
        read(5,9000,iostat=io) inrec
-       reccount = reccount + 1
+       if(io.lt. 0) eof = .true.
        print *,reccount
+       reccount = reccount + 1
+!       print *,reccount
        write(6,9003) inrec
 !      reccount = reccount + 1
        read(5,9000,iostat=io) inrec
+       if (io .lt.0) eof = .true.
+       print *, reccount
        reccount = reccount + 1
-       print *,  reccount
  9000  format(a80)
  9002  format(a4)      
- 9003  format(" ",a80)    
-       if(io  .lt. 0) eof =.true.
-       yearx = year
-       write(year,9002) inrec(19:22)
-       write(6,9002) inrec(19:22)
-!       write(6,9003) inrec
-       print *,reccount
-        yearx = year
-
-        do while (.not. eof .and. (inrec(19:22 ).eq.yearx))
-        write(6,9002) inrec(19:22)
-        write(6,9003)  inrec
-        print *, reccount
+ 9003  format(" ",a80)  
+ 9004  format(a2)
+!       read(year,9002) inrec(19:22)
+!       read(month,9004) inrec(24:25)
+!       read(day,9004) inrec(27:28)
+!       write(6,9002) inrec(19:22)
         yearx = inrec(19:22)
-
-
+!       write(6,9003) inrec
+!       print *,eof, inrec(19:22), yearx
+        do while (.not. eof .and. (inrec(19:22 ).eq.yearx))
+!       print *,eof,inrec(19:22), yearx
+!        write(6,9002) inrec(19:22)
+        write(6,9003)  inrec
+!        read(year,9002) inrec(19:22)
+!        read(month,9004) inrec(24:25)
+!        read(day,9004) inrec(27:28)
+!       print *, reccount, year, month,day
+        yearx = inrec(19:22)
         read(5,9000,iostat=io) inrec
-        reccount = reccount + 1
+        if(io .lt. 0)eof = .true.
+!        yearx = inrec(19:22)
         print *,reccount
-        if(io .lt.0)eof =.true.
-      
+        reccount = reccount + 1
+!        print *,reccount
+!        if(io .lt.0)eof =.true.
+    
        end do
-!       print *, reccount
-        end program
+       end
