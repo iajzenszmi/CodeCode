e969750f61881243474bda43ef2038bb445bdf23
Ian Martin Ajzenszmidt <iajzenszmi@gmail.com>
2021-09-17 14:08:22 +1000

    Add files via upload

---
+++ parabola.f
@@ -0,0 +1,15 @@
+        integer x(10)
+        integer y(10)
+        integer i
+        open(unit=48,file="para.txt")
+        do i = 1, 10, 1
+           x(i) = i
+           y(i) = (x(i) ** 2) + 10
+        enddo
+        print *,x,y
+        do i=1,10
+        write(48,*)x(i),y(i)
+        enddo
+        close(48)
+        call system('gnuplot -p plot2.plt')
+        end program
