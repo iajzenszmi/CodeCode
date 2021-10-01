95744b72341dcfebabb4ffbf50cd5ba876b83a96
Ian Martin Ajzenszmidt <iajzenszmi@gmail.com>
2021-10-01 16:17:10 +1000

    Add files via upload

--- sqrt.f
+++ sqrt.f
@@ -1,12 +1,8 @@
-                        read(5,1)x
-1                       format(f10.5)
-                        a = x/2
-2                       b=(x/a+a)/2
-                        c=b-a
-                        if(c.lt.0)c=-c
-                        if(c.lt.10.e-6)go to 3
-                        a = b
-                        goto 2
-3                       write(6,1)b
-                        stop
-                        end                                                                       
+           integer icount
+            icount = 1
+            do while (icount .le. 10)
+              print *, (sqrt(real(icount)) )
+              icount = icount + 1                 
+            enddo
+            end program
+            
