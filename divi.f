b2e26642929d29d92d3faea2ea0a7bb7652b4c0f
Ian Martin Ajzenszmidt <iajzenszmi@gmail.com>
2021-10-05 21:51:54 +1100

    Add files via upload

---
+++ divi.f
@@ -0,0 +1,6 @@
+               integer icount
+               do icount=1,10,1
+               write(6,9010) "i must wear a mask at all times"
+ 9010          format(" ",a70)
+               end do
+               end program 
