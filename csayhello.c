5ab5c87c03800b7dc0be9c6141a8e0833890ec14
Ian Martin Ajzenszmidt <iajzenszmi@gmail.com>
2021-11-05 14:57:00 +1100

    Add files via upload

---
+++ csayhello.c
@@ -0,0 +1,7 @@
+/* Program to speak through loudspeakers the words "hello world". Written in C 
+ * language by Ian Martin Ajzenszmidt Nov 2021 Melbourne Australia "*/
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+   system("espeak 'hello world'");
+}
